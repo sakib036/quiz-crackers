8779f62041666d4976570423754f69cd8c909028
Md. Nazmus Sakib <sakib036@gmail.com>
2022-10-12 14:21:40 +0600

    rechart-color

--- src/component/ReChart/ReChart.js
+++ src/component/ReChart/ReChart.js
@@ -3,16 +3,20 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsi
 
 const ReChart = ({ data }) => {
     return (
-        <div className='md:w-2/3 mx-auto'>
+        
+        <div  className=' md:w-2/3 mx-auto'>
             
+            <ResponsiveContainer width="100%" height={500}>
                 <LineChart width={500} height={500} data={data}>
-                    <XAxis dataKey="name" />
-                    <YAxis />
+                    <CartesianGrid stroke="#4aa86e" />
+                    <XAxis stroke="#fc03e3" dataKey="name" />
+                    <YAxis stroke="#fc03e3" />
                     <Tooltip />
-                    <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+                    <Line type="monotone" dataKey="total" stroke="#fc03e3" />
 
                 </LineChart>
-               
+                </ResponsiveContainer>
+                
 
         </div>
     );
